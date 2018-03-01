60ff35aefef1fb92f82f4a1c65f3e554bf654de0
Jason <84407852@qq.com>
2018-03-01 14:16:33 +0800

    session config

--- config/config.php
+++ config/config.php
@@ -29,23 +29,56 @@ return [
         ]
     ],
 
+    'session' => [
+        'adapter' => 'files', // Files, Memcache, Libmemcached, Redis
+
+        // file
+        // 'uniqueId' => 'my-app-1',
+
+        // memcache
+        // 'host' => '127.0.0.1',
+        // 'port' => 11211,
+        // 'persistent' => true,
+        // 'lifetime' => 3600,
+        // 'prefix' => 'prefix_'
+        
+        // libmemcached
+        // 'servers' => array(
+        //     ['host' => 'localhost', 'port' => 11211, 'weight' => 1],
+        //     // ['host' => 'localhost', 'port' => 11212, 'weight' => 1],
+        // ),
+        // 'client' => array(
+        //     Memcached::OPT_HASH => Memcached::HASH_MD5,
+        //     Memcached::OPT_PREFIX_KEY => 'prefix_',
+        // ),
+        
+        // redis
+        // "host"       => "127.0.0.1",
+        // "port"       => 6379,
+        // "auth"       => "foobared",
+        // "persistent" => false,
+        // "lifetime"   => 3600,
+        // "prefix"     => "prefix_",
+        // "index"      => 1,
+    ],
+
     'database' => [
         'db'       => [
             'adapter'  => 'Mysql',
             'host'     => '127.0.0.1',
-            'username' => 'username',
-            'password' => 'password',
-            'dbname'   => 'dbname',
-            'prefix'   => 'prefix',
+            'username' => 'root',
+            'password' => 'root123',
+            'dbname'   => 'phalavel',
+            'prefix'   => '',
             'charset'  => 'utf8',
         ],
         'db_slave' => [
             'adapter'  => 'Mysql',
             'host'     => '127.0.0.1',
-            'username' => 'username',
-            'password' => 'password',
-            'dbname'   => 'dbname',
-            'prefix'   => 'prefix',
+            'username' => 'root',
+            'password' => 'root123',
+            'dbname'   => 'phalavel',
+            'prefix'   => '',
             'charset'  => 'utf8',
         ],
     ],
